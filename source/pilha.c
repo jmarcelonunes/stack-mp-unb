f2fcf6f1e98e47016fbea302ed60fde8a30d7c96
Joao Marcelo Nunes Chaves <jmarcelonunes738@gmail.com>
2020-10-10 23:54:08 -0300

    Implementa funções de push e pop e seus respectivos testes

--- source/pilha.c
+++ source/pilha.c
@@ -8,5 +8,35 @@ Stack* create_stack(int maxsize)
     p->actualSize = 0;
     return p;
 }
+void push (Stack *p, void *element){
+    if(p->actualSize < p->maxSize){
+        StackNode* s = (StackNode*) malloc(sizeof(StackNode));
+        ItemType* e = (ItemType*)malloc(sizeof(element));
+        e->data = element;
+        s->itemType = e;
+        s->next = p->top;
+        p->actualSize++;
+        p->top = s;
+    }
+    else{
+        printf("Pilha está cheia!\n");
+    }
 
+}
+
+void* pop(Stack *p){
+    if(p->top != NULL){
+        StackNode* s = p->top;
+        void *e = s->itemType->data;
+        p->top = s->next;
+        p->actualSize--;
+        free(s);
+        return e;
+    }else{
+        printf("Pilha está vazia.\n");
+        int i = -1;
+        void *r = &i;
+        return r;
+    }
+}
 
--- source/pilha_teste.cpp
+++ source/pilha_teste.cpp
@@ -18,3 +18,48 @@ TEST_CASE("Criar uma pilha vazia com um tamanho máximo de elementos")
   REQUIRE( stack->maxSize == 4 );
   REQUIRE( stack->actualSize == 0 );
 }
+TEST_CASE("Empilhar um elemento do tipo inteiro")
+{
+  Stack *stack;
+  int item = 5;
+  stack = create_stack(3);
+  push(stack, &item);
+
+  REQUIRE( stack != NULL );
+  REQUIRE( stack->actualSize == 1 );
+  REQUIRE( stack->top->itemType->data == &item);
+}
+
+TEST_CASE("Empilha dois inteiros e desempilha um elemento da pilha")
+{
+  Stack *stack;
+  int item1 = 5;
+  int item2 = 2;
+  void * popped_item;
+
+  stack = create_stack(3);
+  push(stack, &item1);
+  push(stack, &item2);
+  popped_item = pop(stack);
+  REQUIRE( stack != NULL );
+  REQUIRE( stack->actualSize == 1 );
+  REQUIRE( stack->top->itemType->data == &item1);
+  REQUIRE(*(int *)popped_item == item2);
+}
+
+TEST_CASE("Empilha um inteiro e um char e retira desempilha o elemento tipo char")
+{
+  Stack *stack;
+  int item1 = 5;
+  char item2 = 'c';
+  void * popped_item;
+
+  stack = create_stack(3);
+  push(stack, &item1);
+  push(stack, &item2);
+  popped_item = pop(stack);
+  REQUIRE( stack != NULL );
+  REQUIRE( stack->actualSize == 1 );
+  REQUIRE( stack->top->itemType->data == &item1);
+  REQUIRE(*(char *)popped_item == item2);
+}
