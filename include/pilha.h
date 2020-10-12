6b04ae5e46d36623952260722ad02b2c8bcccfb8
Joao Marcelo Nunes Chaves <jmarcelonunes738@gmail.com>
2020-10-12 16:25:32 -0300

    Fixes

---
+++ include/pilha.h
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct stackNode StackNode;
+typedef struct stack Stack;
+typedef struct itemType ItemType;
+
+struct itemType {
+    void* data;
+};
+
+struct stackNode {
+    ItemType* itemType;
+    StackNode* next;
+};
+
+struct stack {
+	StackNode* top; //implementação por lista encadeada
+    int maxSize;
+    int actualSize;
+    ItemType* vector; //implementação por vetor
+};
+
+Stack* create_stack(int);
+void push(Stack *, void *);
+void* pop(Stack *);
+void* top(Stack *);
+bool set_size(Stack *, int);
+bool is_full(Stack *);
+bool is_empty(Stack *);
+void destroy_stack(Stack **);
